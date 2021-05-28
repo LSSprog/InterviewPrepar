e7a8f5e230d0fae01ff6de928e9a1d18c45415c7
LSSprog <fedexss@bk.ru>
2021-05-28 19:46:09 +0300

    ДЗ к 1 уроку (first commit)

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+//        1. Создать builder для класса Person со следующими полями: String firstName, String lastName,
+//        String middleName, String country, String address, String phone, int age, String gender.
+//        2. Описать ошибки в коде (см. задание в методичке) и предложить варианты оптимизации.
+//        3. Написать пример кода, который реализует принцип полиморфизма, на примере фигур —
+//        круг, квадрат, треугольник.
     }
 }
