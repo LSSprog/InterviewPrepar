b6230b210de97b4e6a1c6e9b6d2b5f76db04e32b
LSSprog <fedexss@bk.ru>
2021-05-31 15:45:20 +0300

    ДЗ к 2 уроку (пока заглушка)

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+//        1. Реализовать основные методы связанного списка.
+//        2. Реализовать основные методы ArrayList.
     }
 }
