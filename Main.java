f65cc4e1f1247adb313f502238ce96a6d0f3425e
LSSprog <fedexss@bk.ru>
2021-06-22 14:51:31 +0300

    ДЗ к 6 уроку (не собирается war файл, и по ссылке http://localhost:8080/hello - выдаёт 404 ошибку)

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-//        1. Установить и настройте Apache Tomcat.
-//        2. Написать сервлет, который выводит надпись Hello World.
+//        + 1. Установить и настройте Apache Tomcat.
+//        + 2. Написать сервлет, который выводит надпись Hello World.
 //        3. Сконфигурировать его так, чтобы при обращении localhost:8080/hello в окне браузера
 //        появлялась надпись HelloWorld.
     }
